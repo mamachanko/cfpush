03891811c7ab12d194b2a2f305c6108fb9724a42
mamachanko <sugardubz@gmail.com>
2019-03-01 17:54:43 +0100

    abort shipping if there are uncommitted changes

--- scripts/ship.sh
+++ scripts/ship.sh
@@ -2,10 +2,40 @@
 
 cd $(dirname $0)
 
+function ensureIndexClean() {
+    if ! git diff --exit-code --cached > /dev/null; then
+        set +x
+        echo "$(tput setab 1)ship abort. there are uncommitted changes in the index.$(tput sgr0)"
+        exit 1
+    fi
+}
+
+function ensureWorkDirClean() {
+    if ! git diff --exit-code > /dev/null \
+        || git status --porcelain | grep '??'; then
+        set +x
+        echo "$(tput setab 1)ship abort. there are uncommitted changes in the working directory.$(tput sgr0)"
+        exit 1
+    fi
+}
+
+function ensureClean() {
+    ensureIndexClean
+    ensureWorkDirClean
+}
+
+function push() {
+    git push
+}
+
+ensureClean
+
 ./destroy.sh
 
 ./deploy.sh
 
-git push
+ensureClean
+
+push
 
 ./destroy.sh
