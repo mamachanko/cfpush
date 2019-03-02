2a7f3b9a8be6d53d867aee29b78c3ff0f2d07138
mamachanko <sugardubz@gmail.com>
2019-03-02 16:03:42 +0100

    get recent messages by order from get-messages.sh

--- scripts/get-messages.sh
+++ scripts/get-messages.sh
@@ -5,5 +5,5 @@ HOST=${1:-http://localhost:8080}
 curl \
     --verbose \
     --request GET \
-    --url "${HOST}/api/messages" \
+    --url "${HOST}/api/messages?page=0&size=20&sort=timestamp,desc" \
     | jq
