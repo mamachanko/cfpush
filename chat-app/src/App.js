93186a85223430cb9e3fcbfd58c0f027f1e38cdd
mamachanko <sugardubz@gmail.com>
2019-03-06 21:04:44 +0100

    apply css baseline to app

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 import Header from "./Header";
 import MessageInput from './MessageInput';
 import MessageListContainer from './MessageListContainer';
+import {CssBaseline} from "@material-ui/core";
 
 export default ({getMessages, postMessage}) => [
+    <CssBaseline key={'css-baseline'}/>,
     <Header key={'header'}/>,
     <MessageListContainer key={'message-list'}
                           getMessages={getMessages}/>,
