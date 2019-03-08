f793ab20c1b1ac22875854086e3f12188bdfa8bc
mamachanko <sugardubz@gmail.com>
2019-03-08 15:59:12 +0100

    use withStyles for Header

--- chat-app/src/Header.js
+++ chat-app/src/Header.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import AppBar from "@material-ui/core/AppBar";
 import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
 
-export default () =>
+const styles = () => ({
+    root: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: '8px 4px'
+    }
+});
+
+const Header = ({classes}) =>
     <AppBar position="static"
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                padding: '8px 4px'
-            }}>
+            className={classes.root}>
         <Typography variant="h6"
                     color="inherit">
             {'Cloud Foundry Tutorial Chat'}
         </Typography>
     </AppBar>;
+
+export default withStyles(styles)(Header);
