696ca7bfa239f1a7148205e76764be188c71b425
mamachanko <sugardubz@gmail.com>
2019-04-03 10:43:18 +0200

    fix linebreak in log method

--- tutor-poc/logging.tsx
+++ tutor-poc/logging.tsx
@@ -3,5 +3,5 @@ import * as fs from 'fs';
 const LOG_FILENAME = '/tmp/app.log';
 
 export const log = (message: string, logFilename: string = LOG_FILENAME): void => {
-	fs.appendFile(logFilename, `${message}/n`, () => { });
+	fs.appendFile(logFilename, `${message}\n`, () => { });
 };
