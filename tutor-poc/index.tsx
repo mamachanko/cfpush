a626145fe90f902bb992111d0f1375050a94e7a3
mamachanko <sugardubz@gmail.com>
2019-04-07 08:45:17 +0200

    log error on failure

--- tutor-poc/index.tsx
+++ tutor-poc/index.tsx
@@ -2,4 +2,8 @@ import {render} from 'ink';
 import * as React from 'react';
 import {App} from './src/app';
 
-render(<App/>);
+const {waitUntilExit} = render(<App/>);
+
+waitUntilExit()
+	.then(() => console.log('🏁'))
+	.catch(error => console.log('🙅🏽‍♀️', error));
