d341b235c437337df5be4246f78d6139315585b8
mamachanko <sugardubz@gmail.com>
2019-04-14 15:11:45 +0200

    don't log state in logging middleware

--- tutor-poc/src/logging-middleware.ts
+++ tutor-poc/src/logging-middleware.ts
@@ -2,8 +2,7 @@ import {Middleware} from 'redux';
 import {logger} from './logging';
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const loggingMiddleware: Middleware = store => next => action => {
-	logger.info(`state: ${JSON.stringify(store.getState())}`);
+export const loggingMiddleware: Middleware = _ => next => action => {
 	logger.info(`action: ${JSON.stringify(action)}`);
 	next(action);
 };
