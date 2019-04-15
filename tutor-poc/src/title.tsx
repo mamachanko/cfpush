ee7a3b3dea352fb891127d89e146dfc65b3e78d5
mamachanko <sugardubz@gmail.com>
2019-04-15 16:44:43 +0200

    restyle Title and add content

--- tutor-poc/src/title.tsx
+++ tutor-poc/src/title.tsx
@@ -1,12 +1,28 @@
 import * as React from 'react';
-import {Color} from 'ink';
-import * as Box from 'ink-box';
+import {Color, Text, Box} from 'ink';
+import * as BorderedBox from 'ink-box';
 
 export const Title = (): React.ReactElement => (
 	<Box
-		borderStyle="round"
-		borderColor="cyan"
+		flexDirection="column"
+		textWrap="wrap"
+		paddingBottom={2}
+		width={80}
 	>
-		Welcome to <Color green>cfpush</Color>
+		<BorderedBox padding={1}>
+			<Text>Welcome to <Color blue bgWhite> cfpush </Color> !</Text>
+		</BorderedBox>
+		<Box marginTop={1}>
+			{'An interactive Cloud Foundry tutorial in your terminal'}
+		</Box>
+		<Box marginTop={1}>
+			{'We will be exploring $(bold \'Cloud Foundry\') and cloud-native computing by deploying a real chat application to Cloud Foundry.'}
+		</Box>
+		<Box marginTop={1}>
+			{'Are you ready? We can\'t wait. Let\'s go!'}
+		</Box>
+		<Box marginTop={1}>
+			{'press <enter> to start'}
+		</Box>
 	</Box>
 );
