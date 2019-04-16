a71f485f9d346855b937373c18252a5c9d990892
mamachanko <sugardubz@gmail.com>
2019-04-16 15:57:25 +0200

    explain how to delete space on exit

--- tutor-poc/src/exit-message.tsx
+++ tutor-poc/src/exit-message.tsx
@@ -12,16 +12,18 @@ export const ExitMessage = (): React.ReactElement => (
 		</Box>
 		<Box marginY={1} flexDirection="column">
 			<Box flexGrow={1}>
-			Was there a problem?
+				You have quit the app. Was there a problem?
 			</Box>
 			<Box>
-			Reach out at <Link url="github.com/mamachanko/cfpush/issues">github.com/mamachanko/cfpush/issues</Link>
+				If so, reach out at <Link url="github.com/mamachanko/cfpush/issues">github.com/mamachanko/cfpush/issues</Link>
 			</Box>
 		</Box>
 		<Box marginY={1} textWrap="wrap" width="80">
-		You might want to decommission the <Text italic>cfpush-tutorial</Text> space to not further incur cost against your quota. 💵
+			You might want to decommission the <Text italic>cfpush-tutorial</Text> space to not further incur cost against your quota. 💵 The command for that is:
+		</Box>
+		<Box marginY={1}>
+			<Text bold>cf delete-space -f cfpush-tutorial</Text>
 		</Box>
 		<Box marginY={1}>See you! 👋🏽</Box>
 	</Div>
-
 );
